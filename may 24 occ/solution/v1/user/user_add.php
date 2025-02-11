8daaea6e8343987e03471c27b7aa2028282fb840
Noah Rob <71009736+inarsy@users.noreply.github.com>
2025-02-11 15:07:18 +0000

    Stuff for the stuff

---
+++ may 24 occ/solution/v1/user/user_add.php
@@ -0,0 +1,30 @@
+<?php
+
+
+    if ($_POST['password'] == $_POST['password_check']) {
+        try {  //try this code
+            $hpswd = password_hash($_POST['password'], PASSWORD_DEFAULT);  //has the password
+
+            include '../functions/db_connect.php';
+
+            $sql = "INSERT INTO users (username, password, f_name, s_name, user_type_id) VALUES (?, ?, ?, ?, ?)";  //prepare the sql to be sent
+            $stmt = $conn->prepare($sql); //prepare to sql
+
+            $stmt->bindParam(1, $_POST['username']);  //bind parameters for security
+            $stmt->bindParam(2, $hpswd);
+            $stmt->bindParam(3, $_POST['fname']);
+            $stmt->bindParam(4, $_POST['lname']);
+            $stmt->bindParam(5, $_POST['user_type']);
+
+            $stmt->execute();  //run the query to insert
+
+            header("refresh:5; url=add_user_type.php"); //confirm and redirect
+            echo "<link rel='stylesheet' href='admin_styles.css'>";
+            echo "Successfully registered a new user type";
+        } catch (PDOException $e) { //catch error
+            //header("refresh:4; url=add_user_type.php");
+            echo "<link rel='stylesheet' href='admin_styles.css'>";
+            echo "Error: " . $e->getMessage();
+            echo "failed to register a user type, try again";
+        }
+    }
--- may 24 occ/solution/v1/user/user_reg.php
+++ may 24 occ/solution/v1/user/user_reg.php
@@ -26,13 +26,29 @@ echo "<h4> Add New Admin </h4>";
 
 echo "<br>";
 
-echo "<form action='/action_page.php'>";
+echo "<form action='user_add.php' method='post'>";
 echo   "<label for='username'>Username:</label><br>";
-echo   "<input type='text' id='username' name='username' value='Doe'><br><br>";
+echo   "<input type='text'  name='username' value=''><br><br>";
+
+echo   "<label for='password'>Password:</label><br>";
+echo   "<input type='text' name='password' value=''><br><br>";
+echo   "<label for='password_check'>Confirm Password:</label><br>";
+echo   "<input type='text' name='password_check' value=''><br><br>";
+
 echo   "<label for='fname'>First name:</label><br>";
-echo   "<input type='text' id='fname' name='fname' value='John'><br>";
+echo   "<input type='text' name='fname' value='John'><br>";
 echo   "<label for='lname'>Last name:</label><br>";
-echo   "<input type='text' id='lname' name='lname' value='Doe'><br><br>";
+echo   "<input type='text' name='lname' value='Doe'><br><br>";
+
+echo   "<label for='cars'>Choose a car:</label>";
+
+echo    "<select name='user_type'>";
+echo    "<option value='1''>Consumer</option>";
+echo    "<option value='2'>Business</option>";
+echo    "<option value='3'>Charity</option>";
+echo    "<option value='4'>Education</option>";
+echo    "</select>";
+
 echo   "<input type='submit' value='Submit'>";
 echo "</form>" ;
 
